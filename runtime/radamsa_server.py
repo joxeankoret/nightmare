fe1ef0bf452a2c4d13f8ee07619a79bb3b9f47d6
Joxean <joxean@coseinc.com>
2016-03-03 19:52:58 +0100

    Added an HTTP based Radamsa server

--- runtime/radamsa_server.py
+++ runtime/radamsa_server.py
@@ -7,12 +7,12 @@ Copyright (c) 2016, Joxean Koret
 Description:
 
 While radamsa offers a way to listen for network connections, there is
-no way to tell what seed we want to use for that specific request. Due
-to this reason, reproducibility is a bit more complex. This server needs
+no way to tell what seed we want to use for one specific request. Due to
+this reason, reproducibility is a bit more complex. This server needs
 both a file extension and a seed in order to serve a sample, forcing to
 always have a way to reproduce test-cases. Also, likely due to a bug in
 Python using subprocess.check_output with radamsa, for some unknown
-reason, may cause weird problems (for example, Python might receive the
+reasons, may cause weird problems (for example, Python might receive the
 SIGSTP signal while or after executing the radamsa command). As so, I 
 think it's better to have a separate component that you can just query
 instead of having to add that logic into your CGI or whatever fuzzer
@@ -22,13 +22,20 @@ How to use it:
 
 At the server-side, run a command similar to this one:
 
-$ python radamsa_server.py <port>
+  $ python radamsa_server.py <port>
 
 At the client-side, perform a HTTP query similar to this one:
 
-$ curl http://server:port/file_extension?seed=value
+  $ curl http://server:port/file_extension?seed=value
 
-That's all!
+The radamsa_server.py script will find the samples of that specific
+file extension in $NIGHTMARE_PATH/samples/<file_extension>. For example,
+if you have the path /home/user/nightmare/samples/doc, and your server
+is listening at port 9090 you will need to issue a HTTP query like this
+one:
+
+  $ curl http://localhost:9090/doc?seed=your_seed
+
 """
 
 import re
@@ -52,7 +59,7 @@ class index:
     if i.seed is None:
       return "No seed specified"
     
-    if not re.match("[a-z0-9]+", extension, re.IGNORECASE):
+    if not re.match("[a-z0-9-_]+", extension, re.IGNORECASE):
       return "Invalid extension"
     
     path = os.path.join(BASE_PATH, extension)
