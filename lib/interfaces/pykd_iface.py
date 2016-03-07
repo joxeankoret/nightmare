66d8267eb1509571630928ade7c6f239af6d1043
forb1dden <jason.kratzer@gmail.com>
2016-03-07 13:17:34 -0500

    Minor bug introduced in cpu autokill

--- lib/interfaces/pykd_iface.py
+++ lib/interfaces/pykd_iface.py
@@ -251,7 +251,7 @@ class CWinDbgInterface(object):
       self.handler = ExceptionHandler()
 
     if self.timeout is not None:
-      if self.timeout.lower() == "auto":
+      if type(self.timeout) is str and self.timeout.lower() == "auto":
         self.thread = Thread(target=self.check_cpu)
         self.thread.start()
       else:
